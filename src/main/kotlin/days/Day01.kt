9a660515d9b75961438b0290c28b9c129f0425d0
Julia Kim <jkim@messerfinancial.com>
2022-12-01 12:40:40 -0500

    day 1

---
+++ src/main/kotlin/days/Day01.kt
@@ -0,0 +1,31 @@
+fun main() {
+    fun mapElvesToCaloriesCounted(input: List<String>): HashMap<Int, Int> {
+        var i = 1
+        val hm: HashMap<Int, Int> = hashMapOf()
+        input.forEach { calories ->
+            if (calories.isBlank()) {
+                i++
+                return@forEach
+            }
+            hm[i] = (hm[i] ?: 0) + calories.toInt()
+        }
+        return hm
+    }
+
+    fun part1(input: List<String>): Int {
+        return mapElvesToCaloriesCounted(input).values.max()
+    }
+
+    fun part2(input: List<String>): Int {
+        return mapElvesToCaloriesCounted(input).values.sortedDescending().take(3).sum()
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day01_test")
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
+
+    val input = readInput("Day01")
+    println(part1(input))
+    println(part2(input))
+}
