de90da22a195dedd6cba47b8c320158cd0319e57
Julia Kim <26291536+julia-kim@users.noreply.github.com>
2022-12-06 07:02:59 -0500

    start day 5

--- src/main/kotlin/days/Day05.kt
+++ src/main/kotlin/days/Day05.kt
@@ -3,8 +3,25 @@ package days
 import readInput
 
 fun main() {
-    fun part1(input: List<String>): Int {
-        return 0
+    fun part1(input: List<String>): String {
+        var message = ""
+        val stacks = mapOf(
+            1 to ArrayDeque(listOf('Z', 'N')),
+            2 to ArrayDeque(listOf('M', 'C', 'D')),
+            3 to ArrayDeque(listOf('P'))
+        )
+        val rearrangementProcedure = input.subList(5, input.size)
+        rearrangementProcedure.forEach {
+            var (qty, from, to) = it.split(" ").mapNotNull { if (it.toIntOrNull() != null) it.toInt() else null }
+            while (qty > 0) {
+                val crate =stacks[from]!!.removeLast()
+                stacks[to]!!.add(crate)
+                qty--
+            }
+        }
+        println(stacks)
+        message = "${stacks[1]!!.removeLast()}${stacks[2]!!.removeLast()}${stacks[3]!!.removeLast()}"
+        return message
     }
 
     fun part2(input: List<String>): Int {
@@ -12,7 +29,7 @@ fun main() {
     }
 
     val testInput = readInput("Day05_test")
-    check(part1(testInput) == 0)
+    check(part1(testInput) == "CMZ")
     check(part2(testInput) == 0)
 
     val input = readInput("Day05")
