8de8dd32113634b9151e809c87e77980e068e90d
Julia Kim <jkim@messerfinancial.com>
2022-12-08 17:24:58 -0500

    day 8 part 2 wip

--- src/main/kotlin/days/Day08.kt
+++ src/main/kotlin/days/Day08.kt
@@ -2,7 +2,9 @@ package days
 
 import readInput
 
+
 fun main() {
+
     fun part1(input: List<String>): Int {
         var array: Array<IntArray> = arrayOf()
         val rows = input[0].length
@@ -27,12 +29,36 @@ fun main() {
                 if (visible) visibleTrees++
             }
         }
-        println(visibleTrees)
         return visibleTrees
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        var array: Array<IntArray> = arrayOf()
+        val rows = input[0].length
+        val cols = input.size
+        input.forEach {
+            val intArray = it.map(Character::getNumericValue).toIntArray()
+            array += intArray
+        }
+        val columns = List(cols) { col -> array.map { it[col] } }
+        var scenicScore: MutableList<Int> = mutableListOf()
+        array.forEachIndexed { i, row ->
+            row.forEachIndexed { j, height ->
+
+                val right = row.take(j).reversed().takeIf { it.isNotEmpty() }
+                val r = right?.indexOfFirst { it >= height }?.let { right.take(it)?.size ?: 0 }
+                val left = row.takeLast(rows - j - 1).takeIf { it.isNotEmpty() }
+                val l = left?.takeWhile { it < height }?.size ?: 0
+                val top = columns[j].take(i).reversed().takeIf { it.isNotEmpty() }
+                val t = top?.takeWhile { it < height }?.size ?: 0
+                val bottom = columns[j].takeLast(cols - i - 1).takeIf { it.isNotEmpty() }
+                val b = bottom?.takeWhile { it < height }?.size ?: 0
+                val score = r * l * t * b
+                scenicScore.add(score)
+            }
+        }
+        println(scenicScore)
+        return scenicScore.max()
     }
 
     val testInput = readInput("Day08_test")
