f71ecc6764ca384b90eaea3645821d3fefdc41ac
Julia Kim <26291536+julia-kim@users.noreply.github.com>
2022-12-08 22:23:28 -0500

    add day 9 files

---
+++ src/main/kotlin/days/Day09.kt
@@ -0,0 +1,21 @@
+package days
+
+import readInput
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+    val testInput = readInput("Day09_test")
+    check(part1(testInput) == 0)
+    check(part2(testInput) == 0)
+
+    val input = readInput("Day09")
+    println(part1(input))
+    println(part2(input))
+}
