1d267a8f86ef25e5c9f0cbb010ae3a0b85df212b
Julia Kim <jkim@messerfinancial.com>
2022-12-14 17:29:25 -0500

    day 14 part 1 wip

--- src/main/kotlin/days/Day14.kt
+++ src/main/kotlin/days/Day14.kt
@@ -21,8 +21,30 @@ fun main() {
         }
     }
 
-    fun part1(input: List<String>): Int {
-        var sandCount = 0
+    fun fallingSand(sand: Point, cave: MutableList<Point>, caveBottom: Int): Boolean {
+        val oneBelow = Point(sand.x, sand.y + 1)
+        val leftBelow = Point(sand.x - 1, sand.y + 1)
+        val rightBelow = Point(sand.x + 1, sand.y + 1)
+        println(listOf(oneBelow, leftBelow, rightBelow))
+        when {
+            sand.y == caveBottom -> {
+                cave.add(sand)
+                return true
+            }
+
+            !cave.contains(oneBelow) -> fallingSand(oneBelow, cave, caveBottom)
+            !cave.contains(leftBelow) -> fallingSand(leftBelow, cave, caveBottom)
+            !cave.contains(rightBelow) -> fallingSand(rightBelow, cave, caveBottom)
+            else -> {
+                cave.add(sand)
+                return true
+            }
+        }
+        return false
+    }
+
+    fun part1(input: List<String>): Long {
+        var sandCount = 0L
         val pourPoint = Point(500, 0)
         val cave = mutableListOf(pourPoint)
         input.forEach { line ->
@@ -35,25 +57,24 @@ fun main() {
                 var dx = secondPoint.x - firstPoint.x
                 var dy = secondPoint.y - firstPoint.y
                 while (dx != 0) {
-                    if (dx < 0) cave.add(Point(firstPoint.x--, firstPoint.y)) else cave.add(
-                        Point(
-                            firstPoint.x++,
-                            firstPoint.y
-                        )
-                    )
+                    if (dx < 0) cave.add(Point(firstPoint.x--, firstPoint.y)) else
+                        cave.add(Point(firstPoint.x++, firstPoint.y))
                     dx = secondPoint.x - firstPoint.x
                 }
                 while (dy != 0) {
-                    if (dy < 0) cave.add(Point(firstPoint.x, firstPoint.y--)) else cave.add(
-                        Point(
-                            firstPoint.x,
-                            firstPoint.y++
-                        )
-                    )
+                    if (dy < 0) cave.add(Point(firstPoint.x, firstPoint.y--)) else
+                        cave.add(Point(firstPoint.x, firstPoint.y++))
                     cave.add(firstPoint)
                     dy = secondPoint.y - firstPoint.y
                 }
             }
+        }
+        val caveBottom = cave.maxOf { it.y }
+        val restingSand = mutableListOf<Point>()
+        var abyss = false
+        while (sandCount < 26) {
+            val sand = Point(500, 0)
+            if (fallingSand(sand, cave, caveBottom)) sandCount++
         }
         printCave(cave.distinctBy { it.x to it.y })
         return sandCount
@@ -65,7 +86,7 @@ fun main() {
     }
 
     val testInput = readInput("Day14_test")
-    check(part1(testInput) == 24)
+    check(part1(testInput) == 0L)
     check(part2(testInput) == 0)
 
     val input = readInput("Day14")
