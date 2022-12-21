68b5e6854b60493096bce3774818374cb447a641
Julia Kim <jkim@messerfinancial.com>
2022-12-21 10:11:27 -0500

    day 21 part 2

--- src/main/kotlin/days/Day21.kt
+++ src/main/kotlin/days/Day21.kt
@@ -1,6 +1,7 @@
 package days
 
 import readInput
+import kotlin.math.absoluteValue
 
 fun main() {
     fun part1(input: List<String>): Long {
@@ -35,14 +36,84 @@ fun main() {
         return monkeyNumbers["root"]!!
     }
 
-    fun part2(input: List<String>): Int {
+    fun part2(input: List<String>): Long {
+        val monkeyNumbers = mutableMapOf<String, Long>()
+        val monkeyOperations = mutableMapOf<String, String>()
+        input.forEach {
+            val (name, job) = it.split(": ")
+            if (name == "humn") return@forEach
+            if (job.all { it.isDigit() }) monkeyNumbers[name] = job.toLong()
+            else monkeyOperations[name] = job
+        }
+        val monkeyRoot = mutableListOf(monkeyOperations["root"]!!.take(4), monkeyOperations["root"]!!.takeLast(4))
+        monkeyOperations.remove("root")
+        var magicNumber: Long = 0
+        while (magicNumber == 0L) {
+            monkeyOperations.forEach {
+                val (name, job) = it
+                if (!monkeyNumbers.containsKey(name)) {
+                    val monkey1 = job.take(4)
+                    val monkey2 = job.takeLast(4)
+                    val mathOperator = job[5]
+                    if (monkeyNumbers.containsKey(monkey1) && monkeyNumbers.containsKey(monkey2)) {
+                        monkeyNumbers[name] = when (mathOperator) {
+                            '+' -> monkeyNumbers[monkey1]!! + monkeyNumbers[monkey2]!!
+                            '-' -> monkeyNumbers[monkey1]!! - monkeyNumbers[monkey2]!!
+                            '*' -> monkeyNumbers[monkey1]!! * monkeyNumbers[monkey2]!!
+                            '/' -> monkeyNumbers[monkey1]!! / monkeyNumbers[monkey2]!!
+                            else -> {
+                                throw IllegalStateException()
+                            }
+                        }
+                        if (monkeyRoot.contains(name)) magicNumber = monkeyNumbers[name]!!
+                        monkeyRoot.remove(name)
+                    }
+                }
+            }
+        }
+        monkeyNumbers.keys.forEach {
+            monkeyOperations.remove(it)
+        }
+        monkeyNumbers[monkeyRoot[0]] = magicNumber
+        while (!monkeyNumbers.containsKey("humn")) {
+            monkeyOperations.forEach {
+                val (name, job) = it
+                val monkey1 = job.take(4)
+                val monkey2 = job.takeLast(4)
+                val mathOperator = job[5]
+                when {
+                    monkeyNumbers.containsKey(name) && monkeyNumbers.containsKey(monkey1) -> {
+                        monkeyNumbers[monkey2] = when (mathOperator) {
+                            '+' -> monkeyNumbers[name]!! - monkeyNumbers[monkey1]!!
+                            '-' -> (monkeyNumbers[name]!! - monkeyNumbers[monkey1]!!).absoluteValue
+                            '*' -> monkeyNumbers[name]!! / monkeyNumbers[monkey1]!!
+                            '/' -> monkeyNumbers[name]!! * (1 / monkeyNumbers[monkey1]!!)
+                            else -> {
+                                throw IllegalStateException()
+                            }
+                        }
+                    }
 
-        return 0
+                    monkeyNumbers.containsKey(name) && monkeyNumbers.containsKey(monkey2) -> {
+                        monkeyNumbers[monkey1] = when (mathOperator) {
+                            '+' -> monkeyNumbers[name]!! - monkeyNumbers[monkey2]!!
+                            '-' -> monkeyNumbers[name]!! + monkeyNumbers[monkey2]!!
+                            '*' -> monkeyNumbers[name]!! / monkeyNumbers[monkey2]!!
+                            '/' -> monkeyNumbers[name]!! * monkeyNumbers[monkey2]!!
+                            else -> {
+                                throw IllegalStateException()
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return monkeyNumbers["humn"]!!
     }
 
     val testInput = readInput("Day21_test")
     check(part1(testInput) == 152L)
-    check(part2(testInput) == 0)
+    check(part2(testInput) == 301L)
 
     val input = readInput("Day21")
     println(part1(input))
