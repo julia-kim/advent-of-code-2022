65188040b3b37c7cb73ef5f2c7422587528d61a4
Julia Kim <jkim@messerfinancial.com>
2022-12-30 13:42:39 -0500

    day 12 part 2

--- src/main/kotlin/days/Day12.kt
+++ src/main/kotlin/days/Day12.kt
@@ -22,12 +22,8 @@ fun main() {
             val neighbors =
                 distances.filter { ((it.key.x - current.key.x).absoluteValue == 1 && it.key.y == current.key.y) || ((it.key.y - current.key.y).absoluteValue == 1 && it.key.x == current.key.x) }
                     .filter {
-                        var currentElevation = heightmap[current.key]!!
-                        var neighborElevation = heightmap[it.key]!!
-                        if (heightmap[current.key]!! == 'S') currentElevation = 'a'
-                        if (heightmap[it.key]!! == 'S') neighborElevation = 'a'
-                        if (heightmap[current.key]!! == 'E') currentElevation = 'z'
-                        if (heightmap[it.key]!! == 'E') currentElevation = 'z'
+                        val currentElevation = heightmap[current.key]!!
+                        val neighborElevation = heightmap[it.key]!!
                         neighborElevation <= currentElevation + 1
                     }
                     .toMutableMap()
@@ -46,7 +42,41 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        /* Dijkstra's algorithm */
+        val heightmap = input.flatMapIndexed { i, string ->
+            string.mapIndexed { j, char -> Point(j, i) to char }
+        }.toMap().toMutableMap()
+        val s = heightmap.entries.find { it.value == 'S' }!!.key
+        val start = heightmap.entries.find { it.value == 'E' }!!.key
+        heightmap[s] = 'a'
+        heightmap[start] = 'z'
+        val target = heightmap.entries.filter { it.value == 'a' }.map { it.key }
+        val distances = heightmap.map { it.key to Int.MAX_VALUE }.toMap().toMutableMap()
+        distances[start] = 0
+        val unvisited = distances.keys.toMutableList()
+        while (unvisited.isNotEmpty()) {
+            val current = distances.filter { unvisited.contains(it.key) }.minBy { it.value }
+            val neighbors =
+                distances.filter { ((it.key.x - current.key.x).absoluteValue == 1 && it.key.y == current.key.y) || ((it.key.y - current.key.y).absoluteValue == 1 && it.key.x == current.key.x) }
+                    .filter {
+                        val currentElevation = heightmap[current.key]!!
+                        val neighborElevation = heightmap[it.key]!!
+                        neighborElevation - currentElevation >= -1
+                    }
+                    .toMutableMap()
+            neighbors.remove(current.key)
+            neighbors.forEach { neighbor ->
+                if (distances[neighbor.key]!! > distances[current.key]!! + 1) {
+                    distances[neighbor.key] = distances[current.key]!! + 1
+                }
+            }
+            if (target.contains(current.key)) {
+                return distances[current.key]!!
+            }
+            unvisited.remove(current.key)
+        }
+        println(distances.filter { target.contains(it.key) })
+        return distances.filter { target.contains(it.key) }.values.min()
     }
 
     val testInput = readInput("Day12_test")
